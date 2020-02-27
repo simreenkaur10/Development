aa9daf477237d07d8d55520ad3343a6ff556cd61
Simreen Kaur <simreenkaurbindra10@gmail.com>
2020-02-27 22:01:49 +0530

    new file

--- Lecture 1/Activity/commands/treefy.js
+++ Lecture 1/Activity/commands/treefy.js
@@ -1,5 +1,29 @@
+let fs = require("fs")
+let path = require("path")
 module.exports.treefy=function(){
     let src = arguments[0];
     let dest = arguments[1];
+    treefy1(src,dest,root)
     console.log("treefy command has been called");
+}
+
+
+function treefy1(src,dest,node){
+    
+    if(node.isFile==true){
+       let oldFile=path.join(src,node.newname)
+       let newFile=path.join(dest,node.name)
+       fs.copyFileSync(oldFile,newFile)
+    
+    }
+    else{
+        let dirName = path.join(dest,node.name)
+        fs.mkdirSync(dirName)
+        for(let i=0;i<node.children.length;i++){
+            
+            treefy1(src,dirName,node.children[i])
+            
+        }
+        
+    }
 }
--- Lecture 1/Activity/commands/untreefy.js
+++ Lecture 1/Activity/commands/untreefy.js
@@ -4,21 +4,31 @@ let uniqid =require("uniqid")
 module.exports.untreefy=function(){
     let src = arguments[0];
     let dest = arguments[1];
-untreefy1(src,dest)
+    let root ={};
+untreefy1(src,dest,root);
+console.log(root)
+fs.writeFileSync(path.join(dest,"metadata.json"),JSON.stringify(root))
 console.log("all files copied")}
 
-function untreefy1(src,dest){
+function untreefy1(src,dest,node){
     let ans = fs.lstatSync(src).isDirectory();
     if(ans==false){
        let uid=uniqid()
+       node.isFile = true;
+       node.name = path.basename(src)
+       node.newname = uid
        fs.copyFileSync(src,path.join(dest,uid)) 
     }
     else{
-        
-        let children = fs.readdirSync(src);
-        for(let i=0;i<children.length;i++){
-            let child= path.join(src,children[i])
-            untreefy1(child,dest)
+        node.isFile=false
+        node.name=path.basename(src)
+        node.children =[];
+        let childrens = fs.readdirSync(src);
+        for(let i=0;i<childrens.length;i++){
+            let childobj ={}
+            let child= path.join(src,childrens[i])
+            untreefy1(child,dest,childobj)
+            node.children.push(childobj)
         }
         
     }
